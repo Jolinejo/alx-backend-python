fa0ef0f3ae02924c1e2037c54ef81a8be37ccc3d
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-15 23:21:13 +0200

    trying 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+sum module
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    returns sum of list
+    """
+    sum = 0.0
+    for num in input_list:
+        sum += num
+    return sum
