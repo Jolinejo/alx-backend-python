8798c1d9361610a937e088def39537d99e44f0b3
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-24 00:45:48 +0000

    task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """wair for delay"""
     for i in range(10):
         delay = random.uniform(0, 10)
